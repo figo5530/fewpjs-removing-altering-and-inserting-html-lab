81c84afe92fea15b64451cfcd9bcb0b601708914
figo5530 <figooooo0221@gmail.com>
2021-04-15 18:04:43 +0800

    commit tree 680888b1cb44ac0085e3c0f1f4583ef0ca18785b

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let ele = document.body.querySelector("main")
+ele.remove()
+
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion"
